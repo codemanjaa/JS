b639df2f6097ff4b20f274152d4a0450494f608b
Codemanja <codemanjaa@gmail.com>
2017-09-11 17:47:23 +0300

    create the game initiation

---
+++ pickgame/script.js
@@ -0,0 +1,89 @@
+
+
+var score, roundScore, activePlayer;
+score =[0,0];
+activePlayer = 0;
+roundScore = 0;
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+
+
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+var dice = Math.floor(Math.random() * 6)+1;
+   // document.querySelector('#current-'+activePlayer).textContent = dice;
+   document.querySelector('#current-'+activePlayer).innerHTML = '<em>' + dice +'</em>';
+   var diceDOM = document.querySelector('.dice');
+   diceDOM.style.display = 'block';
+   diceDOM.src = 'dice-' + dice + '.png';
+
+//if (dice !== 1) type coercion
+   if (dice !== 1){
+       roundScore += dice;
+       document.querySelector('#current-'+activePlayer).textContent = roundScore;
+   }
+   else{
+      
+        nextPlayer();
+
+       }
+   
+
+   
+});
+
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //Add current score to the Global score
+    score[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+
+    if(score[activePlayer] >= 10){
+
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('#name-'+activePlayer).textContent = 'Winner!!!'
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        document.querySelector('.btn-roll').style.display = 'none';
+
+    }
+    else {
+
+         nextPlayer();
+    }
+  
+
+
+
+});
+
+
+function nextPlayer(){
+
+     activePlayer === 0 ? activePlayer =1 : activePlayer = 0;
+       roundScore = 0;
+       document.getElementById('current-0').textContent = '0';
+       document.getElementById('current-1').textContent = '0';
+       document.querySelector('.player-0-panel').classList.toggle('active');
+       document.querySelector('.player-1-panel').classList.toggle('active');
+       document.querySelector('.dice').style.display = 'none';
+       //document.querySelector('.player-0-panel').classList.remove('active');
+       //document.querySelector('.player-1-panel').classList.add('active');
+
+       
+
+}
